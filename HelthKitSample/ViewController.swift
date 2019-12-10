38add54ad1b6e4e798db8b023d167c5d5b341baf
seiyamaeda <s.maezono.73@gmail.com>
2019-12-10 14:47:36 +0900

    Initial Commit

---
+++ HelthKitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelthKitSample
+//
+//  Created by Seiya Maeda on 2019/12/10.
+//  Copyright © 2019 Seiya Maeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
